12cafefe32f3defc0f03fd32ccfa7538417ae43d
nugrohoFZ <nugroho.grprogames@gmail.com>
2024-05-06 18:30:43 +0700

    Latihan Pertemuan 6

---
+++ Latihan Pertemuan 5/latihan1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan6;
+import javax.swing.*;
+/**
+ *
+ * @author uSeR
+ */
+public class latihan1 {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        
+        myWindow = new JFrame();
+        myWindow.setSize(300, 20);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+    }
+}
